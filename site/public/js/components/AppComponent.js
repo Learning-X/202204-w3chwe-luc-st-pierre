89fc134094b0ba380332e1ac5729515114e32048
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-23 16:54:22 +0200

    Fixed indentation from github actions

--- site/public/js/components/AppComponent.js
+++ site/public/js/components/AppComponent.js
@@ -34,44 +34,43 @@ export default class AppComponent extends Component {
 
     gridlist.innerHTML = `
     <li class="grid-item">
-              <div class="card-container">
-                <a href="#" class="card-container__link">
-                  <div class="card">
-                    <div class="card__box"></div>
-                    <div class="card__image"></div>
-                    <div class="card-media__root">
-                      <div class="root-container">
-                        <div class="root-container__left space">
-                          <i class="icon space">icon</i>
-                          <span class="icon-info space">info</span>
-                        </div>
-
-                        <div class="root-container__right space">
-                          <i class="icon space">icon</i>
-                          <span class="icon-info space">info</span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-
-                <div class="card__content">
-                  <a href="#" class="card__content-wrapper">
-                    <h6 class="card__content-title">title</h6>
-                  </a>
-
-                  <div class="card__content-info">
-                    <span class="info">info</span>
-
-                    <div class="btn-container">
-                      <button type="button" class="btn">
-                        <span class="btn-text">view</span>
-                      </button>
-                    </div>
-                  </div>
-                </div>
+      <div class="card-container">
+        <a href="#" class="card-container__link">
+          <div class="card">
+          <div class="card__box"></div>
+          <div class="card__image"></div>
+          <div class="card-media__root">
+            <div class="root-container">
+              <div class="root-container__left space">
+                <i class="icon space">icon</i>
+                <span class="icon-info space">info</span>
               </div>
-       </li>
+
+              <div class="root-container__right space">
+                <i class="icon space">icon</i>
+                <span class="icon-info space">info</span>
+              </div>
+            </div>
+          </div>
+         </div>
+        </a>
+
+        <div class="card__content">
+          <a href="#" class="card__content-wrapper">
+            <h6 class="card__content-title">title</h6>
+          </a>
+
+          <div class="card__content-info">
+            <span class="info">info</span>
+            <div class="btn-container">
+              <button type="button" class="btn">
+                <span class="btn-text">view</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
     `;
   }
 }
