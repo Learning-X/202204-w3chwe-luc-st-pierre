41419760ef876eb1d34a0761efd7f3798cba360f
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-23 15:15:50 +0200

    Temporary commented out import  github action error

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,4 +1,4 @@
-import getFilterPokemonData from "./localAPI.js";
+// import getFilterPokemonData from "./localAPI.js";
 
 // const pokemons = await getFilterPokemonData();
 // console.log(pokemons);
