2174e306b1ae0a4e4c32a3cbcef51d0cb7f6a50b
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-23 14:55:02 +0200

    Renamed index for localAPI and created a new index.js

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,26 +1,4 @@
-const pokeApiClient = "https://pokeapi.co/api/v2/pokemon";
-
-const fetchPokemons = async () => {
-  const response = await fetch(pokeApiClient);
-  const jsonData = await response.json();
-  const data = jsonData.results;
-  // console.log(data);
-  return data;
-};
-
-const getPokemonData = async () => {
-  const allPokemons = await fetchPokemons(pokeApiClient);
-
-  const promise = allPokemons.map(async (pokemon) => {
-    const responseURLs = await fetch(pokemon.url);
-    const jsonResponseURLs = await responseURLs.json();
-    return jsonResponseURLs;
-  });
-
-  const pokemonList = await Promise.all(promise);
-  // console.log(pokemonList);
-  return pokemonList;
-};
+import getPokemonData from "./localAPI.js";
 
 const pokemons = await getPokemonData();
 // console.log(pokemons);
