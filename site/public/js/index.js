05d12484c30a2875795d7e8d64996d18da7d6fd8
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-23 10:30:51 +0200

    Tested PokeAPI pokemon endpoint from d ocumentation

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,1 +1,10 @@
+const pokeApiClient = "https://pokeapi.co/api/v2/pokemon";
 
+const getAllPokemons = async () => {
+  const response = await fetch(pokeApiClient);
+  const data = await response.json();
+  // console.log(data);
+  return data;
+};
+
+getAllPokemons();
