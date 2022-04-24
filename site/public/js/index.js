b92cec5a6805cd3a96e83cc7bbf14d6adf86aacf
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-24 17:53:43 +0200

    Created class PokemonApi for crud and deployed to heroku

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,11 +1,16 @@
 import ClientApi from "./ClientApi.js";
 import AppComponent from "./components/AppComponent.js";
+import PokemonApi from "./PokemonApi.js";
 
-const pokemonApiClient = new ClientApi(
+const clientApi = new ClientApi(
   "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
+);
+
+const pokemonApi = new PokemonApi(
+  "https://pokemon-api-luc.herokuapp.com/pokemon"
 );
 
 const mainContainer = document.querySelector(".app");
 
 // eslint-disable-next-line no-new
-new AppComponent(mainContainer, pokemonApiClient);
+new AppComponent(mainContainer, pokemonApi, clientApi);
